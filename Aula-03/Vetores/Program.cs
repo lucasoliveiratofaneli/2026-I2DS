51d6ba31879242b897738209d359e9db4518a72c
lucasoliveiratofaneli <lucasoliveirasenaijau@gmail.com>
2026-08-04 14:59:33 -0300

    Repetição e Vetores

---
+++ Aula-03/Vetores/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* Vetores e Matrizes
+            Vetor => é uma estrutura de dados utilizada para armazenar varios valores de um mesmo tipo dentrio de uma unica variavel
+
+            Sintaxe: tipo[] nome vetor =new tipo[tamanho]; 
+        */
+
+        /* Iniciar um vetor -> lista de frutas */
+
+        string[] listaFrutas = {"Maça","Manga","Morango","Melancia","Melão"};
+
+        Console.WriteLine("Acessar o  terceiro elemento das lista");
+        Console.WriteLine(listaFrutas[2]);
+
+        Console.WriteLine("Alterar o segundo elemento da lista: ");
+        listaFrutas[1] = "Banana";
+
+        for(int indice = 0; indice < listaFrutas.Count(); indice++ )
+        {
+            Console.WriteLine(listaFrutas[indice]);
+        }
+
+        /*******************************************************************************/
+
+        foreach (string fruta in listaFrutas)
+        {
+            Console.WriteLine(fruta);
+        }
+
+
+
+    }
+}
