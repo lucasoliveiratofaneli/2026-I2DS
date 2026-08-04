ae803403e5512219d48bd62407a9b7926f82f9c5
lucasoliveiratofaneli <lucasoliveirasenaijau@gmail.com>
2026-08-04 17:06:05 -0300

    Exercicios

---
+++ Aula-03/Exercicios/Exercicio-01/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Exercicios 1
+        int celsius;
+        Console.Write("Digite a temperatura em Graus Celsius: ");
+        celsius = int.Parse(Console.ReadLine());
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine("A conversão de Celsius para Fahrenheit é: " + fahrenheit);
+
+    }
+}
