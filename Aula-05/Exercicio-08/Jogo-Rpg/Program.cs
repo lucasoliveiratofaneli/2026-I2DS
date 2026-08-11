fd14ff0e7098f973f72690e60ac3112c59a88e99
lucasoliveiratofaneli <lucasoliveirasenaijau@gmail.com>
2026-08-11 16:55:48 -0300

    Aula 05

---
+++ Aula-05/Exercicio-08/Jogo-Rpg/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+    }
+
+    public class Personagem
+    {
+        public string Nome {get; private set;}
+        public int Nivel {get; private set;}
+        public int Forca {get; private set;}
+        public int Agilidade {get; private set;}
+        public int Inteligencia {get; private set;}
+        public int Vida {get; private set;}
+        
+    }
+
+    public class Classes
+    {
+        public string Mago {get; private set;}
+        public string Elfo {get; private set;}
+        public string Cavaleiro {get; private set;}
+    }
+}
