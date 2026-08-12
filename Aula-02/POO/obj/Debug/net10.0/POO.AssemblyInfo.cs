17283a1d5e9ab3b7e8486085ca17101f87d29e40
lucasoliveiratofaneli <lucasoliveirasenaijau@gmail.com>
2026-08-12 08:37:24 -0300

    Finalizando a Aula-05

--- Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
+++ Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd14ff0e7098f973f72690e60ac3112c59a88e99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40064c9f1c24b28aee3ed635ca8e1db0e0c1aa07")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aula-03/Condicionais/obj/Debug/net10.0/Condicionais.AssemblyInfo.cs
+++ Aula-03/Condicionais/obj/Debug/net10.0/Condicionais.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Condicionais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd14ff0e7098f973f72690e60ac3112c59a88e99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40064c9f1c24b28aee3ed635ca8e1db0e0c1aa07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Condicionais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Condicionais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
